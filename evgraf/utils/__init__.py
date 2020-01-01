6deea5af9872ee3d2a363990165a9d5691cce138
pmla <peter.mahler.larsen@gmail.com>
2020-01-01 21:55:36 +0100

    moved wrapping into evgrafcpp

--- evgraf/utils/__init__.py
+++ evgraf/utils/__init__.py
@@ -2,8 +2,7 @@ from .minkowski_reduction import minkowski_reduce
 from .pbc import pbc2pbc
 from .axis_permutation import permute_axes
 from .standardization import standardize, standardize_cell
-from .plotting import plot_atoms
 from .rotation_matrix import rotation_matrix
 
 __all__ = ['minkowski_reduce', 'pbc2pbc', 'permute_axes', 'standardize',
-           'plot_atoms', 'rotation_matrix', 'standardize_cell']
+           'rotation_matrix', 'standardize_cell']
--- evgraf/wallpaper/__init__.py
+++
@@ -1,4 +0,0 @@
-#from .layer_standardization import standardize_chain
-from .wallpaper import get_wallpaper_distance
-
-__all__ = ['get_wallpaper_distance']
