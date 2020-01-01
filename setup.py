c366192b6de5113048fca6e6b1d647a2d17e8f2e
pmla <peter.mahler.larsen@gmail.com>
2020-01-01 23:28:47 +0100

    0.1.4

--- setup.py
+++ setup.py
@@ -5,20 +5,22 @@ from setuptools import Extension, find_packages, setup
 
 evgrafcpp_module = Extension(
     'evgrafcpp',
-    sources=['src/evgrafcpp_module.cpp',
-             'src/rectangular_lsap.cpp',
-             'src/crystalline.cpp',
-             'src/wrap_positions.cpp',
-             'src/lup_decomposition.cpp',
-             'src/matrix_vector.cpp'],
+    ['src/evgrafcpp_module.cpp',
+     'src/rectangular_lsap.cpp',
+     'src/crystalline.cpp',
+     'src/wrap_positions.cpp',
+     'src/lup_decomposition.cpp',
+     'src/matrix_vector.cpp',
+    ],
     include_dirs=[os.path.join(numpy.get_include(), 'numpy'),
-                  'src'],
+                  'src',
+    ],
     language='c++'
 )
 
 major_version = 0
 minor_version = 1
-subminor_version = 3
+subminor_version = 4
 version = '%d.%d.%d' % (major_version, minor_version, subminor_version)
 
 setup(name='evgraf',
@@ -30,7 +32,6 @@ setup(name='evgraf',
       url='https://github.com/pmla/evgraf',
       ext_modules=[evgrafcpp_module],
       install_requires=['numpy',
-                        'scipy',
                         'ase>=3.18.1'],
       packages=find_packages()
       )
