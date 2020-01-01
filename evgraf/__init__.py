1b1608985abae5f27990d09ed1aa69761635863a
pmla <peter.mahler.larsen@gmail.com>
2020-01-01 22:04:15 +0100

    removed chain functions

--- evgraf/__init__.py
+++ evgraf/__init__.py
@@ -1,6 +1,4 @@
 from .crystal_reduction import find_crystal_reductions
 from .inversion_symmetry import find_inversion_symmetry
-from .chains import standardize_chain, calculate_rmsd_chain
 
-__all__ = ['find_crystal_reductions', 'find_inversion_symmetry',
-           'standardize_chain', 'calculate_rmsd_chain']
+__all__ = ['find_crystal_reductions', 'find_inversion_symmetry']
