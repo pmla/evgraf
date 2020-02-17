8fa9d73ae4d45ae625d22403e61c328970d6a87d
pmla <peter.mahler.larsen@gmail.com>
2020-02-17 13:32:44 +0100

    added header files to setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,17 @@ from setuptools import Extension, find_packages, setup
 
 evgrafcpp_module = Extension(
     'evgrafcpp',
-    ['src/evgrafcpp_module.cpp',
-     'src/rectangular_lsap.cpp',
-     'src/crystalline.cpp',
-     'src/wrap_positions.cpp',
+    ['src/crystalline.cpp',
+     'src/crystalline.h',
+     'src/evgrafcpp_module.cpp',
      'src/lup_decomposition.cpp',
+     'src/lup_decomposition.h',
      'src/matrix_vector.cpp',
+     'src/matrix_vector.h',
+     'src/rectangular_lsap.cpp',
+     'src/rectangular_lsap.h',
+     'src/wrap_positions.cpp',
+     'src/wrap_positions.h',
     ],
     include_dirs=[os.path.join(numpy.get_include(), 'numpy'),
                   'src',
@@ -20,7 +25,7 @@ evgrafcpp_module = Extension(
 
 major_version = 0
 minor_version = 1
-subminor_version = 5
+subminor_version = 6
 version = '%d.%d.%d' % (major_version, minor_version, subminor_version)
 
 setup(name='evgraf',
